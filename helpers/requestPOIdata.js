fdb35ffc4b8f2708a98c9d586f6797e66853e757
heladolk <heladolk@yahoo.com>
2016-07-21 16:26:56 -0700

    update default value

--- helpers/requestPOIdata.js
+++ helpers/requestPOIdata.js
@@ -2,7 +2,10 @@
 
 var request = require('request');
 
-function requestPOIdata(latitude, longitude, poiDistance=10) {
+function requestPOIdata(latitude, longitude, poiDistance) {
+  if(!poiDistance) {
+    poiDistance = 10;
+  }
   let url = `http://terminal2.expedia.com/x/geo/features?within=${poiDistance}mi&lng=${longitude}&lat=${latitude}&type=point_of_interest&verbose=3&lcid=1033&apikey=${process.env.EXPEDIA_API_KEY}`;
 
   return promisifyGet(url);
